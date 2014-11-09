9fecc3bdc1921cb3f36e5cee6b11a5d0dc9ee8bc
Tom <tengli01@villanova.edu>
2014-11-08 22:21:03 -0500

    updates

--- Unity App/Assets/CubeScript.cs
+++ Unity App/Assets/CubeScript.cs
@@ -18,6 +18,12 @@ public class CubeScript : MonoBehaviour
 		float yrot = server.p.rotations [1];
 		float zrot = server.p.rotations [2];
 
+		float xpos = server.p.accelerations [0];
+		float ypos = server.p.accelerations [1];
+		float zpos = server.p.accelerations [2];
+
 		transform.localEulerAngles = new Vector3 (xrot,yrot,zrot);
+
+		transform.localPosition = new Vector3 (xpos, ypos, zpos);
 	}
 }
